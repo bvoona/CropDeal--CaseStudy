9c5a33344d6c433f601d5cbaefa49cc2ee0a5d20
bvoona <115697446+bvoona@users.noreply.github.com>
2023-01-07 18:45:35 +0530

    Add files via upload

---
+++ CaseStudy/AdminService/src/main/java/com/cropdeal/adminservice/repositpry/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cropdeal.adminservice.repositpry;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cropdeal.adminservice.model.UserModel;
+@Repository
+public interface UserRepository extends MongoRepository<UserModel,String>{
+
+	UserModel findByUsername(String username);
+
+}
